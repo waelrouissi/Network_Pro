d5f25c87c1bcb62f2aed07c5eccfe125f1b422c6
salsabiil <salsabil.dridi1@gmail.com>
2019-07-17 17:54:26 +0200

    trying to pull , no real changes were made

--- ProfessionalNetwork.Data/Migrations/201907152012447_K.Designer.cs
+++ ProfessionalNetwork.Data/Migrations/201907152012447_K.Designer.cs
@@ -7,13 +7,13 @@ namespace ProfessionalNetwork.Data.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.1.3-40302")]
-    public sealed partial class version2 : IMigrationMetadata
+    public sealed partial class K : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(version2));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(K));
         
         string IMigrationMetadata.Id
         {
-            get { return "201907151737572_version2"; }
+            get { return "201907152012447_K"; }
         }
         
         string IMigrationMetadata.Source
